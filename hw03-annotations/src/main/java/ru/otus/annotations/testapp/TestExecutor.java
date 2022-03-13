375ea72c4c50416a7dac4d9797f51273e6683580
DiyarovV <v.diyarovv@gmail.com>
2022-03-13 15:42:23 +0300

    After методы исполняются в любом случае. Убрал goto

--- hw03-annotations/src/main/java/ru/otus/annotations/testapp/TestExecutor.java
+++ hw03-annotations/src/main/java/ru/otus/annotations/testapp/TestExecutor.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 
 public class TestExecutor {
     private String className;
-    private int failedTestsCount;
+    private static int failedTestsCounter;
 
     public TestExecutor(String className) {
         this.className = className;
@@ -21,55 +21,80 @@ public class TestExecutor {
     public void run() throws ClassNotFoundException {
         Class<?> cls = Class.forName(className);
 
-        List<Method> methods = Arrays.asList(cls.getDeclaredMethods());
-        List<Method> beforeMethods = methods.stream()
-                .filter(m -> m.isAnnotationPresent(Before.class))
-                .peek(m -> m.setAccessible(true))
-                .collect(Collectors.toList());
-        List<Method> afterMethods = methods.stream()
-                .filter(m -> m.isAnnotationPresent(After.class))
-                .peek(m -> m.setAccessible(true))
-                .collect(Collectors.toList());
-        List<Method> testMethods = methods.stream()
-                .filter(m -> m.isAnnotationPresent(Test.class))
-                .peek(m -> m.setAccessible(true))
-                .collect(Collectors.toList());
+        List<Method> beforeMethods = getBeforeMethodsAndSetAccessible(cls);
+        List<Method> testMethods = getTestMethodsAndSetAccessible(cls);
+        List<Method> afterMethods = getAfterMethodsAndSetAccessible(cls);
 
-        tests:
         for (Method T : testMethods) {
-            boolean failed = false;
+            boolean failCounterIncremented = false;
             Object o = ReflectionHelper.instantiate(cls);
 
-            for (Method B : beforeMethods) {
+            boolean runBeforeMethodsSuccessed = runBeforeMethods(beforeMethods, o);
+
+            if (runBeforeMethodsSuccessed) {
                 try {
-                    ReflectionHelper.callMethod(o, B.getName());
-                } catch (Exception e ) {
+                    ReflectionHelper.callMethod(o, T.getName());
+                } catch (Exception e) {
                     System.out.println(e.getCause().toString());
-                    failedTestsCount++;
-                    continue tests;
+                    failCounterIncremented = true;
+                    failedTestsCounter++;
                 }
             }
 
-            try {
-                ReflectionHelper.callMethod(o, T.getName());
-            } catch (Exception e ) {
-                System.out.println(e.getCause().toString());
-                failed = true;
-                failedTestsCount++;
-            }
-
-            for (Method A : afterMethods) {
-                try {
-                    ReflectionHelper.callMethod(o, A.getName());
-                } catch (Exception e ) {
-                    System.out.println(e.getCause().toString());
-                    if (!failed) { failedTestsCount++; }
-                }
-            }
+            runAfterMethods(afterMethods, o, !runBeforeMethodsSuccessed || failCounterIncremented);
         }
 
         System.out.println("Всего тестов: " + testMethods.size());
-        System.out.println("Пройдено неуспешно: " + failedTestsCount);
-        System.out.println("Пройдено успешно: " + (testMethods.size() - failedTestsCount));
+        System.out.println("Пройдено неуспешно: " + failedTestsCounter);
+        System.out.println("Пройдено успешно: " + (testMethods.size() - failedTestsCounter));
     }
+
+    public static boolean runBeforeMethods(List<Method> methods, Object o) {
+        for (Method B : methods) {
+            try {
+                ReflectionHelper.callMethod(o, B.getName());
+            } catch (Exception e ) {
+                System.out.println(e.getCause().toString());
+                failedTestsCounter++;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void runAfterMethods(List<Method> methods, Object o, boolean failCounterIncremented) {
+        for (Method A : methods) {
+            try {
+                ReflectionHelper.callMethod(o, A.getName());
+            } catch (Exception e ) {
+                System.out.println(e.getCause().toString());
+                if (!failCounterIncremented) { failedTestsCounter++; }
+            }
+        }
+    }
+
+    public static List<Method> getBeforeMethodsAndSetAccessible(Class cls) {
+        List<Method> methods = Arrays.asList(cls.getDeclaredMethods());
+        return methods.stream()
+                .filter(m -> m.isAnnotationPresent(Before.class))
+                .peek(m -> m.setAccessible(true))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Method> getTestMethodsAndSetAccessible(Class cls) {
+        List<Method> methods = Arrays.asList(cls.getDeclaredMethods());
+        return methods.stream()
+                .filter(m -> m.isAnnotationPresent(Test.class))
+                .peek(m -> m.setAccessible(true))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Method> getAfterMethodsAndSetAccessible(Class cls) {
+        List<Method> methods = Arrays.asList(cls.getDeclaredMethods());
+        return methods.stream()
+                .filter(m -> m.isAnnotationPresent(After.class))
+                .peek(m -> m.setAccessible(true))
+                .collect(Collectors.toList());
+    }
+
 }
