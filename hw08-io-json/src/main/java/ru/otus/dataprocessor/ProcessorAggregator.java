0f5882ef95b182519a0fc28dbdcf2d947f02f0c1
DiyarovV <v.diyarovv@gmail.com>
2022-05-22 06:08:13 +0300

    Домашнее задание №8 После Лекции №16 IO Сериализация.

--- hw08-io-json/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw08-io-json/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -4,12 +4,21 @@ import ru.otus.model.Measurement;
 
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
-        //группирует выходящий список по name, при этом суммирует поля value
-        return null;
+        Map<String, Double> result = new TreeMap<>();
+        for (Measurement measurement : data) {
+            String name = measurement.getName();
+            Double value = measurement.getValue();
+            if (result.containsKey(name)) {
+                value = result.get(name) + value;
+            }
+            result.put(name, value);
+        }
+        return result;
     }
 }
--- hw08-io-json/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-io-json/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,17 +1,29 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
 import ru.otus.model.Measurement;
 
+import java.io.*;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
+    private String fileName;
 
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
-    public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+    public List<Measurement> load() throws FileProcessException {
+        Gson gson = new Gson();
+        try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        ) {
+            Measurement[] measurements = gson.fromJson(bufferedReader, Measurement[].class);
+            return List.of(measurements);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
