4d3fd67e7c96d0be69d06797d86af049877d1cb3
Divanches <divanches@mail>
2022-05-24 21:53:09 +0300

    Домашнее задание №9 После Лекции №18 JDBC. In progress..

--- hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
+++ hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
@@ -1,32 +1,49 @@
 package ru.otus.jdbc.mapper;
 
+import ru.otus.crm.annotations.Id;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EntityClassMetaDataImpl implements EntityClassMetaData {
+    private Class entityClass;
+
+    public EntityClassMetaDataImpl(Class entityClass) {
+        this.entityClass = entityClass;
+    }
+
     @Override
     public String getName() {
-        return null;
+        return entityClass.getSimpleName();
     }
 
     @Override
     public Constructor getConstructor() {
+        try {
+            return entityClass.getDeclaredConstructor();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
         return null;
     }
 
     @Override
     public Field getIdField() {
-        return null;
+        List<Field> fields = Arrays.asList(entityClass.getDeclaredFields());
+        return fields.stream().filter(f -> f.isAnnotationPresent(Id.class)).collect(Collectors.toList()).get(0);
     }
 
     @Override
     public List<Field> getAllFields() {
-        return null;
+        return List.of(entityClass.getDeclaredFields());
     }
 
     @Override
     public List<Field> getFieldsWithoutId() {
-        return null;
+        List<Field> fields = Arrays.asList(entityClass.getDeclaredFields());
+        return fields.stream().filter(f -> !f.isAnnotationPresent(Id.class)).collect(Collectors.toList());
     }
 }
--- hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntitySQLMetaDataImpl.java
+++ hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntitySQLMetaDataImpl.java
@@ -1,23 +1,39 @@
 package ru.otus.jdbc.mapper;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EntitySQLMetaDataImpl implements EntitySQLMetaData {
+    private final EntityClassMetaData entityClassMetaData;
+    private String className;
+    private String idFieldName;
+
+    public EntitySQLMetaDataImpl(EntityClassMetaData entityClassMetaData) {
+        this.entityClassMetaData = entityClassMetaData;
+        this.className = entityClassMetaData.getName();
+        this.idFieldName = entityClassMetaData.getIdField().getName();
+    }
+
     @Override
     public String getSelectAllSql() {
-        return null;
+        return String.format("select * from %s", className);
     }
 
     @Override
     public String getSelectByIdSql() {
-        return null;
+        return String.format("select * from %s where %s = ?", className, idFieldName);
     }
 
     @Override
     public String getInsertSql() {
-        return null;
+        List<Field> fields = entityClassMetaData.getAllFields();
+        String values = String.join(",", fields.stream().map(f -> f.getName()).collect(Collectors.toList()));
+        return String.format("insert into %s (%s) values (?)", className, values);
     }
 
     @Override
     public String getUpdateSql() {
-        return null;
+        return String.format("update %s set ? = ? where ? = ?", className);
     }
 }
