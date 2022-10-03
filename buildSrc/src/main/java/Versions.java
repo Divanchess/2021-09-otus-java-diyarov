14c60f3fd1ad0bb1ba86e9b97961d43545cf9866
Divanches <divanches@mail>
2022-10-03 22:09:57 +0600

    Домашнее задание №17 Grpc

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -6,10 +6,12 @@ public interface Versions {
     String testcontainers = "1.15.3";
     String reflections = "0.10.2";
     String jetty = "11.0.7";
-    String gson = "2.9.0";
     String freemarker = "2.3.31";
     String jackson = "2.13.0";
     String springBootVersion = "2.6.2";
     String jsr305 = "3.0.2";
     String lombok = "1.18.22";
+    String grpc = "1.44.1";
+    String errorProneAnnotations = "2.11.0";
+    String tomcatAnnotationsApi = "6.0.53";
 }
